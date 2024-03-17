426a4ff725269d0339817f2ab8918ff581dd38e3
roney.dsilva <roney.dsilva@cdmx.in>
2024-03-18 00:16:32 +0530

    intial commit

---
+++ dmx-custom-s3-upload.js
@@ -0,0 +1,351 @@
+dmx.Actions({
+  "s3.custom-s3-upload": function (t) {
+      var e = this.parse(t.input),
+          i = this.parse(t.url),
+          s = document.getElementById(e).files[0];
+      return new Promise((function (t, e) {
+          var n = new XMLHttpRequest;
+          n.onerror = e, n.onabort = e, n.ontimeout = e, n.onload = t, n.open("PUT", i), n.setRequestHeader("Content-Type", s.type), n.send(s)
+      }))
+  }
+}),
+dmx.Component("custom-s3-upload", {
+  initialData: {
+      data: null,
+      file: null,
+      state: {
+          idle: !0,
+          ready: !1,
+          uploading: !1,
+          done: !1
+      },
+      uploadProgress: {
+          position: 0,
+          total: 0,
+          percent: 0
+      },
+      lastError: {
+          status: 0,
+          message: "",
+          response: null
+      }
+  },
+  attributes: {
+    url: {
+        type: String,
+        default: null
+    },
+    prop: {
+        type: String,
+        default: "url"
+    },
+    accept: {
+        type: String,
+        default: null
+    },
+    autoupload: {
+        type: Boolean,
+        default: !1
+    }
+},
+methods: {
+  abort: function () {
+      this.abort()
+  },
+  reset: function () {
+      this.reset()
+  },
+  select: function () {
+      this.input.click()
+  },
+  upload: function () {
+      this.upload()
+  }
+},
+
+
+  parseFileData: async function (fieldId) {
+    const parseCSV = (csvData) => {
+      return new Promise((resolve, reject) => {
+        Papa.parse(csvData, {
+          header: true,
+          dynamicTyping: true,
+          complete: function (results) {
+            resolve(results.data);
+          },
+          error: function (error) {
+            reject(error.message);
+          }
+        });
+      });
+    };
+  
+    const parseExcel = (excelData) => {
+      return new Promise((resolve, reject) => {
+        const workbook = new ExcelJS.Workbook();
+        workbook.xlsx.load(excelData)
+          .then(() => {
+            const worksheet = workbook.getWorksheet(1);
+            const data = [];
+            const headers = [];
+  
+            worksheet.eachRow((row, rowNumber) => {
+              if (rowNumber === 1) {
+                row.eachCell((cell) => {
+                  headers.push(cell.value);
+                });
+              } else {
+                const rowData = {};
+                row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+                  rowData[headers[colNumber - 1]] = cell.value;
+                });
+                data.push(rowData);
+              }
+            });
+  
+            resolve(data);
+          })
+          .catch((error) => {
+            reject(error.message);
+          });
+      });
+    };
+  
+    const fileInput = document.getElementById(fieldId);
+    if (!fileInput) {
+      console.error('Field having field Id: '+fieldId+' not found.');
+      return;
+    }
+    const file = fileInput.files[0];
+    if (!file) {
+      console.error('Please select a file.');
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const fileData = e.target.result;
+  
+      try {
+        let parsedData;
+        // Detect the file type based on the file extension or other criteria
+        if (file.name.endsWith('.csv')) {
+          parsedData = await parseCSV(fileData);
+        } else if (file.name.endsWith('.xlsx') || file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+          parsedData = await parseExcel(fileData);
+        } else {
+          console.error('Unsupported file type. Please select a CSV or Excel file.');
+          return;
+        }
+  
+        this.set('fileData', parsedData);
+      } catch (error) {
+        console.error('Error parsing file:', error);
+      }
+    };
+  
+    reader.readAsBinaryString(file);
+  },
+
+  events: {
+    start: Event,
+    done: Event,
+    error: Event,
+    abort: Event,
+    success: Event,
+    upload: ProgressEvent
+},
+  render: function(node) {
+    if (this.$node) {
+      this.$parse();
+    }
+  },
+
+  render: function (t) {
+    this.$node.addEventListener("dragover", this.onDragover.bind(this)), this.$node.addEventListener("drop", this.onDrop.bind(this)), this.$node.addEventListener("click", this.onClick.bind(this)), this.input = document.createElement("input"), this.input.type = "file", this.input.accept = this.props.accept || "*/*", this.input.addEventListener("change", this.onChange.bind(this)), this.xhr = new XMLHttpRequest, this.xhr.addEventListener("abort", this.onAbort.bind(this)), this.xhr.addEventListener("error", this.onError.bind(this)), this.xhr.addEventListener("timeout", this.onTimeout.bind(this)), this.xhr.addEventListener("load", this.onLoad.bind(this)), this.xhr.upload.addEventListener("progress", this.onProgress.bind(this)), this.$parse()
+},
+update: function (t) {
+    this.props.accept != t.accept && (this.input.accept = this.props.accept || "*/*")
+},
+onDragover: function (t) {
+    t.stopPropagation(), t.preventDefault(), t.dataTransfer.dropEffect = 1 == t.dataTransfer.items.length ? "copy" : "none"
+},
+onDrop: function (t) {
+    t.stopPropagation(), t.preventDefault(), 1 == t.dataTransfer.files.length && this.updateFile(t.dataTransfer.files[0])
+},
+onClick: function (t) {
+    this.input.click()
+},
+onChange: function (t) {
+    this.updateFile(t.target.files[0]), this.input.value = "", this.input.type = "", this.input.type = "file"
+},
+onAbort: function (t) {
+    this.set({
+        data: null,
+        state: {
+            idle: !1,
+            ready: !0,
+            uploading: !1,
+            done: !1
+        },
+        uploadProgress: {
+            position: 0,
+            total: 0,
+            percent: 0
+        }
+    }), this.dispatchEvent("abort"), this.dispatchEvent("done")
+},
+onError: function (t) {
+    t instanceof ProgressEvent && (t = "Network error, perhaps no CORS set"), this.set({
+        data: null,
+        state: {
+            idle: !1,
+            ready: !0,
+            uploading: !1,
+            done: !1
+        },
+        uploadProgress: {
+            position: 0,
+            total: 0,
+            percent: 0
+        },
+        lastError: {
+            status: 0,
+            message: t,
+            response: null
+        }
+    }), console.error(t), this.dispatchEvent("error"), this.dispatchEvent("done")
+},
+onTimeout: function (t) {
+    this.onError("Execution timeout")
+},
+onLoad: function (t) {
+    this.xhr.status >= 400 ? this.onError(this.xhr.responseText) : (this.set({
+        state: {
+            idle: !1,
+            ready: !1,
+            uploading: !1,
+            done: !0
+        },
+        uploadProgress: {
+            position: this.file.size,
+            total: this.file.size,
+            percent: 100
+        }
+    }), this.dispatchEvent("success"), this.dispatchEvent("done"))
+},
+onProgress: function (t) {
+    this.set({
+        state: {
+            idle: !1,
+            ready: !1,
+            uploading: !0,
+            done: !1
+        },
+        uploadProgress: {
+            position: t.loaded,
+            total: this.file.size,
+            percent: Math.ceil(t.loaded / t.total * 100)
+        }
+    }), this.dispatchEvent("upload", {
+        lengthComputable: t.lengthComputable,
+        loaded: t.loaded,
+        total: t.total
+    })
+},
+    validate: function (t) {
+      if (t.type.toLowerCase() === 'text/csv') {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var rows = event.target.result.split('\n');
+            var numRows = rows.length - 1; // Subtract header
+            console.log(numRows)
+            if (numRows > 10) { 
+                console.log("CSV file contains more than 10 rows.");
+                return !1
+            }
+        };
+        reader.readAsText(t);
+    }
+    return !this.props.accept || this.props.accept.split(/\s*,\s*/g).some((function (e) {
+        if ("." == e.charAt(0)) {
+            if (t.name.match(new RegExp("\\" + e + "$", "i"))) return !0
+        } else if (/(audio|video|image)\/\*/i.test(e)) {
+            if (t.type.match(new RegExp("^" + e.replace(/\*/g, ".*") + "$", "i"))) return !0
+        } else if (t.type.toLowerCase() == e.toLowerCase()) return !0;
+        return !1
+    }))
+},
+
+updateFile: function (t) {
+  if (this.validate(t)) {
+      var e = {
+          name: t.name,
+          size: t.size,
+          type: t.type,
+          date: (t.lastModified ? new Date(t.lastModified) : t.lastModifiedDate).toISOString(),
+          dataUrl: null
+      }; - 1 === t.type.indexOf("image/") || t.reader || (t.reader = new FileReader, t.reader.onload = function (t) {
+          e.dataUrl = t.target.result, dmx.requestUpdate()
+      }.bind(this), t.reader.readAsDataURL(t)), this.file = t, this.set({
+          file: e,
+          state: {
+              idle: !1,
+              ready: !0,
+              uploading: !1,
+              done: !1
+          }
+      }), this.props.autoupload && this.upload()
+  }
+},
+abort: function () {
+  this.xhr.abort()
+},
+reset: function () {
+  this.abort(), this.file = null, this.set({
+      data: null,
+      file: null,
+      state: {
+          idle: !0,
+          ready: !1,
+          uploading: !1,
+          done: !1
+      },
+      uploadProgress: {
+          position: 0,
+          total: 0,
+          percent: 0
+      },
+      lastError: {
+          status: 0,
+          message: "",
+          response: null
+      }
+  })
+},
+upload: function () {
+  if (this.props.url) {
+      this.set({
+          state: {
+              idle: !1,
+              ready: !1,
+              uploading: !0,
+              done: !1
+          }
+      }), this.dispatchEvent("start");
+      var t = new XMLHttpRequest;
+      t.onabort = this.onAbort.bind(this), t.onerror = this.onError.bind(this), t.onload = this.upload2.bind(this, t), t.open("GET", this.props.url + "?name=" + encodeURIComponent(this.file.name)), t.send()
+  } else this.onError("No url attribute is set")
+},
+upload2: function (t) {
+  try {
+      var e = JSON.parse(t.responseText),
+          i = e[this.props.prop];
+      if (this.set("data", e), this.xhr.open("PUT", i), this.xhr.setRequestHeader("Content-Type", this.file.type), -1 != i.indexOf("x-amz-acl=")) {
+          var s = i.substr(i.indexOf("x-amz-acl=") + 10); - 1 != s.indexOf("&") && (s = s.substr(0, s.indexOf("&"))), this.xhr.setRequestHeader("x-amz-acl", s)
+      }
+      this.xhr.send(this.file)
+  } catch (t) {
+      this.onError(t)
+  }
+}
+});
